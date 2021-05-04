0c61e46e0e13e7ad3ec64f88a56e554632082ce0
Chandrasekhar Reddy <chandrasekhar.reddy@philips.com>
2021-05-04 09:02:12 +0530

    first checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ImageCropDialog } from 'src/image-crop-dialog/image-crop-dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'dailogImageCropper';
+  logo = '';
+  title = 'dailogImageCropper'; 
+
+  constructor(public dialog:MatDialog,
+    private changeDetectorRef: ChangeDetectorRef,){
+
+  }
+
+  fileChangeEvent(event){
+    const dialogRef = this.dialog.open(ImageCropDialog, {
+      data: {image: event}  
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.logo= result;
+      this.changeDetectorRef.markForCheck();
+      this.changeDetectorRef.detectChanges();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ImageCropDialog } from 'src/image-crop-dialog/image-crop-dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageCropDialog
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    ImageCropperModule,
+    BrowserModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
+  exports:[ImageCropDialog],
+  entryComponents:[ImageCropDialog],
   providers: [],
   bootstrap: [AppComponent]
 })
